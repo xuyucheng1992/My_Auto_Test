4b0ac46c2b635eee8691c856f62ebdc4634a37f1
echo <18621289233@163.com>
2021-06-23 18:01:55 +0800

    处理 c_token

--- apis/get_token.py
+++ apis/get_token.py
@@ -9,30 +9,24 @@ from common.com_request import ComRequests
 
 
 class GetToken:
-
-
     @classmethod
     def get_c_token(self):
-        login_params = ComManage().manage_test_params('login.yaml')
-
+        login_params = ComManage().manage_test_params("login.yaml")
         login_params = login_params[0][0]
         resp = ComRequests().send_requests(login_params)
         print(resp.json())
-
+        token =resp.json()["responsebody"]["token"]
+        mid = resp.json()["responsebody"]["mId"]
+        return mid + "_" + token
 
     @classmethod
     def get_p_token(self):
-        login_params = ComManage().manage_test_params('p_login.yaml')
-
+        login_params = ComManage().manage_test_params("p_login.yaml")
         login_params = login_params[0][0]
         resp = ComRequests().send_requests(login_params)
         print(resp.json())
         return resp.json()["access_token"]
 
 
-
-
-
-
-if __name__ =='__main__':
+if __name__ == "__main__":
     print(GetToken.get_p_token())
